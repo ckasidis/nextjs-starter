834dfab4a5ce3386663fad4d77170a37db53d7c8
Kasidis Chantharojwong <me@ckasidis.com>
2023-07-08 23:43:36 +0800

    build: bump dependencies

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,6 +1,5 @@
 module.exports = {
   '*.{js,jsx,ts,tsx}': ['eslint --fix', 'eslint'],
-  '**/*.ts?(x)': () => 'pnpm check-types',
   '*.{html,css,scss,js,jsx,cjs,mjs,ts,tsx,md,mdx,json,yml,yaml,gql,graphql}': [
     'prettier --write',
   ],
